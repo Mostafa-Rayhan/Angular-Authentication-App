22b38bff11729320db56150ded9cd84de4e7abdd
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-09-11 11:03:08 +0600

    reset password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './component/home/home.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { ConfirmotpComponent } from './component/confirmotp/confirmotp.component';
+import { ResetpasswordComponent } from './component/resetpassword/resetpassword.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'register',component:RegisterComponent},
     {path:'login',component:LoginComponent},
     {path:'confirmotp',component:ConfirmotpComponent},
+    {path:'resetpassword',component:ResetpasswordComponent},
 ];
